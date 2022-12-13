2dd32bb428e8b151b8c40e390bbeb43283a6ed3e
tina <tinhinaneachir@gmail.com>
2022-12-13 10:43:21 +0100

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {RxStompService} from "./rx-stomp.service";
+import {rxStompServiceFactory} from "./rx-stomp-service-factory";
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
