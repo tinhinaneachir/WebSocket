6638e0da3b413dba70212c93dbc2262de2cb6d2e
Antoine Desobry <antoine.desobry@cyna-it>
2022-12-14 10:40:56 +0100

    correctif et chat fonctionnel

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -32,6 +32,18 @@ export class MessagesComponent implements OnInit, OnDestroy {
   onSendMessage() {
     let message = this.message?.nativeElement.value;
     let user = this.bogoss?.nativeElement.value;
-    this.rxStompService.publish({ destination: '/topic/demo', body: message, headers:{user:user} });
+    let date = new Date();
+    this.rxStompService.publish({
+      destination: '/topic/demo',
+      body: message,
+      headers:{
+        user:user,
+        time:[
+          date.getHours().toString().padStart(2, '0'),
+          date.getMinutes().toString().padStart(2, '0'),
+          date.getSeconds().toString().padStart(2, '0'),
+        ].join(':')
+      }
+    });
   }
 }
